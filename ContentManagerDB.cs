0afa157a668e2a063c952c76ff8fdb1141d66e12
BenedekBenjamin <BenedekBenjamin@U11WKS04>
2017-06-23 09:22:22 +0200

    CManagerDB LoadElements erstellt!

--- ContentManagerDB.cs
+++ ContentManagerDB.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.OleDb;
 using System.Linq;
 using System.Text;
@@ -16,14 +17,11 @@ namespace WindowsFormsAppZeitgeist
         public List<Karte> KomponentenVorrat { get; set; }
         public List<Karte> DienerVorrat { get; set; }
 
-
         OleDbConnection con;
         OleDbConnectionStringBuilder strbuild;
         OleDbCommand command;
         OleDbDataReader rd;
 
-        //con.ConnectionString = Properties.Settings.Default.DbPath;
-        //con.Open();
         public ContentManagerDB()
         {
             IdeenElemente = new List<Idee>();
@@ -33,6 +31,86 @@ namespace WindowsFormsAppZeitgeist
             IdeenVorrat = new List<Karte>();
             KomponentenVorrat = new List<Karte>();
             DienerVorrat = new List<Karte>();
+
+            con = new OleDbConnection();
+            con.ConnectionString = Properties.Settings.Default.DBPath;
+            con.Open();
+        }
+
+        public void LoadGameElements()
+        {
+            command = con.CreateCommand();
+
+
+            //KomponentenElemente laden
+            command.CommandText = "Component";
+            command.CommandType = CommandType.TableDirect;
+
+            rd = command.ExecuteReader();
+
+            while (rd.Read())
+            {
+                Komponente neu = new Komponente();
+                int i = 0;
+
+                neu.KompIndex = AsValue<int>(rd[i++]);
+                neu.GlobalIndex = AsValue<int>(rd[i++]);
+                neu.Bezeichnung = AsValue<string>(rd[i++]);
+                neu.Quantity = AsValue<int>(rd[i++]);
+
+                KomponentenElemente.Add(neu);
+            }
+            rd.Close();
+
+
+            //IdeeElemente laden
+            command.CommandText = "Idea";
+            command.CommandType = CommandType.TableDirect;
+
+            rd = command.ExecuteReader();
+
+            while (rd.Read())
+            {
+                Idee neu = new Idee();
+                int i = 0;
+
+                neu.IdeeIndex = AsValue<int>(rd[i++]);
+                neu.GlobalIndex = AsValue<int>(rd[i++]);
+                neu.Bezeichnung = AsValue<string>(rd[i++]);
+                neu.Punktewert = AsValue<int>(rd[i++]);
+                neu.Patentwert = AsValue<int>(rd[i++]);
+                neu.Ebene = AsValue<int>(rd[i++]);
+                neu.Quantity = AsValue<int>(rd[i++]);
+            }
+            rd.Close();
+
+
+            //DienerElemente laden
+            command.CommandText = "Idea";
+            command.CommandType = CommandType.TableDirect;
+
+            rd = command.ExecuteReader();
+
+            while (rd.Read())
+            {
+                Diener neu = new Diener();
+                int i = 0;
+
+                neu.DienerIndex = AsValue<int>(rd[i++]);
+                neu.GlobalIndex = AsValue<int>(rd[i++]);
+                neu.Bezeichnung = AsValue<string>(rd[i++]);
+                neu.Quantity = AsValue<int>(rd[i++]);
+            }
+            rd.Close();
+        }
+
+        private T AsValue<T>(object o)
+        {
+            if (o == DBNull.Value)
+            {
+                return default(T);
+            }
+            return (T)o;
         }
     }
 }
